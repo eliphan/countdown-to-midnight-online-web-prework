2e1b9e6f52fa7b81d1859393c4d95f103078c898
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-03 06:51:10 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown (x)
   
   while x > 0
   x -= 1
-  sleep (1)
+
   puts "#{x} SECOND(S)!"
  end
   if x == 0
@@ -12,3 +12,6 @@ def countdown (x)
  end
 
 end
+
+def countdown_with_sleep (number)
+  sleep (1)
