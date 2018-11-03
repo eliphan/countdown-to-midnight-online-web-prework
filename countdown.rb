15c49bcf4c94e6a3139dc94b720ae08f4c312429
generic-thread-7687 <aliceconnic@gmail.com>
2018-11-03 06:48:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown (x)
+  
+  while x > 0
+  x -= 1
+  sleep (1)
+  puts "#{x} SECONDS!"
+ end
+  if x == 0
+  puts "HAPPY NEW YEAR!"
+ end
+
 end
